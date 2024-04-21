4bc2750fb20cb3b66012c467093f304957783a1a
ybchar <uiurihappy@sju.ac.kr>
2024-04-21 16:46:17 +0900

    Project init

---
+++ src/main/java/com/jyujyu/springtest/TestData.java
@@ -0,0 +1,11 @@
+package com.jyujyu.springtest;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class TestData {
+
+	private String name;
+}
---
+++ src/test/java/com/jyujyu/springtest/LombokTestDataTest.java
@@ -0,0 +1,14 @@
+package com.jyujyu.springtest;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class LombokTestDataTest {
+
+	@Test
+	public void testDataTest() {
+		TestData testData = new TestData();
+		testData.setName("Hello jyujyu!");
+		Assertions.assertEquals("Hello jyujyu!", testData.getName());
+	}
+}
