4d9486e465fd166eff06d50eb29a75a9ee51a4d3
ybchar <uiurihappy@sju.ac.kr>
2024-04-30 00:05:23 +0900

    feat: student score entity

---
+++ src/main/java/com/jyujyu/springtest/score/domain/StudentFail.java
@@ -0,0 +1,35 @@
+package com.jyujyu.springtest.score.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@Getter
+@Entity
+@Table(name = "student_fail")
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+public class StudentFail {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "student_fail_id")
+	private Long id;
+
+	@Column(name = "exam")
+	private String exam;
+
+	@Column(name = "student_name")
+	private String studentName;
+
+	@Column(name = "avg_score")
+	private Double avgScore;
+}
---
+++ src/main/java/com/jyujyu/springtest/score/domain/StudentScore.java
@@ -0,0 +1,41 @@
+package com.jyujyu.springtest.score.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@Getter
+@Entity
+@Table(name = "student_score")
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+public class StudentScore {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "student_score_id")
+	private Long id;
+
+	@Column(name = "exam")
+	private String exam;
+
+	@Column(name = "student_name")
+	private String studentName;
+
+	@Column(name = "kor_score")
+	private Integer korScore;
+
+	@Column(name = "eng_score")
+	private Integer engScore;
+
+	@Column(name = "math_score")
+	private Integer mathScore;
+}
