218077934c791c925d7942135b3eba3a3b8d263e
ybchar <uiurihappy@sju.ac.kr>
2024-04-30 18:50:46 +0900

    feat: first save Mock Test

--- src/test/java/com/jyujyu/springtest/score/application/StudentScoreServiceTest.java
+++ src/test/java/com/jyujyu/springtest/score/application/StudentScoreServiceTest.java
@@ -1,4 +1,43 @@
+package com.jyujyu.springtest.score.application;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import com.jyujyu.springtest.score.dao.StudentFailRepository;
+import com.jyujyu.springtest.score.dao.StudentPassRepository;
+import com.jyujyu.springtest.score.dao.StudentScoreRepository;
+
 class StudentScoreServiceTest {
-  
+
+	@Test
+	@DisplayName("첫 번째 성적 저장 테스트")
+	public void firstSaveScoreMockTest() {
+		// given
+		StudentScoreService studentScoreService = new StudentScoreService(
+			// Mock 객체를 StudentScoreService 생성자에 주입
+			Mockito.mock(StudentScoreRepository.class),
+			Mockito.mock(StudentPassRepository.class),
+			Mockito.mock(StudentFailRepository.class)
+		);
+		String studentName = "jyujyu";
+		String exam = "midterm";
+		Integer korScore = 80;
+		Integer engScore = 100;
+		Integer mathScore = 60;
+
+		// when
+		studentScoreService.saveScore(
+			studentName,
+			exam,
+			korScore,
+			engScore,
+			mathScore
+		);
+
+		// then
+
+	}
 }
